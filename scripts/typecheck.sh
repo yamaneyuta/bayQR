42d7084480ec34acaacb929008c15c0b6511481d
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-11-11 15:08:12 +0000

    chore(root): typecheck.shを追加

---
+++ scripts/typecheck.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -euo pipefail
+
+if [ -f "./tsconfig.json" ]; then
+    export PROJECT="./tsconfig.json"
+elif [ -f "./tsconfig.esm.json" ]; then
+    export PROJECT="./tsconfig.esm.json"
+elif [ -f "./tsconfig.cjs.json" ]; then
+    export PROJECT="./tsconfig.cjs.json"
+else
+    echo "[88B44438] No tsconfig file found in $(pwd)."
+    exit 1
+fi
+
+pnpm exec tsc --noEmit --strict --skipLibCheck -p "$PROJECT"
