544a7c38511f8adf5634f12da5b5441ae57e0cbd
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-11-12 14:19:08 +0000

    refactor(value-object): ChianIdクラスのコンストラクタをprivateに変更し、from静的メソッドを追加

--- packages/lib-value-object/src/value-object/ChainId.ts
+++ packages/lib-value-object/src/value-object/ChainId.ts
@@ -6,8 +6,16 @@ export class ChainId extends ValueObject<ChainId> {
 	/** 型区別用のフィールド */
 	private [brand]!: void;
 
-	public constructor(private readonly chainIdValue: number) {
+	private constructor(private readonly chainIdValue: number) {
 		super();
+
+		if (chainIdValue <= 0 || !Number.isInteger(chainIdValue)) {
+			throw new Error(`[19364922] ChainId must be a non-negative integer. ${chainIdValue}`);
+		}
+	}
+
+	public static from(chainIdValue: number): ChainId {
+		return new ChainId(chainIdValue);
 	}
 
 	public get value(): number {
