67a80a65ec213fe626973af6a611d81546b27e92
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-11-12 00:09:06 +0000

    chore(root): ベースとなるjestの設定ファイルを追加

---
+++ config/jest/jest.config.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+
+const config = JSON.parse(fs.readFileSync(`${__dirname}/.swcrc`, "utf-8"));
+module.exports = {
+	transform: {
+		"^.+\\.(t|j)sx?$": ["@swc/jest", { ...config }],
+	},
+};
