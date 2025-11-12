4dcc33b12a9f6320a86b2322a8993a6ad9be213e
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-11-12 14:19:46 +0000

    test(value-object): ChianIdのテスト

---
+++ packages/lib-value-object/src/value-object/ChainId.error.test.ts
@@ -0,0 +1,15 @@
+import { ChainId } from "./ChainId";
+
+describe("[Error] ChainId", () => {
+	const dataset: { chainIdValue: number }[] = [
+		{ chainIdValue: 0 },
+		{ chainIdValue: 3.14 },
+		{ chainIdValue: NaN },
+		{ chainIdValue: Infinity },
+		{ chainIdValue: -42 },
+	];
+	test.each(dataset)(`[DDCB159F] from - $chainIdValue`, async ({ chainIdValue }) => {
+		// ACT & ASSERT
+		expect(() => ChainId.from(chainIdValue)).toThrow(`[19364922]`);
+	});
+});
---
+++ packages/lib-value-object/src/value-object/ChainId.happy.test.ts
@@ -0,0 +1,33 @@
+import { ChainId } from "./ChainId";
+
+describe("[Happy] ChainId", () => {
+	test("[DDCB159F] from", async () => {
+		// ACT
+		const chainId = ChainId.from(42);
+
+		// ASSERT
+		expect(chainId.value).toBe(42);
+	});
+
+	test("[B216D2B7] toString", async () => {
+		// ARRANGE
+		const sut = ChainId.from(42);
+
+		// ACT
+		const str = sut.toString();
+
+		// ASSERT
+		expect(str).toBe("42");
+	});
+
+	test("[384DA641] equals", async () => {
+		// ARRANGE
+		const sut1 = ChainId.from(42);
+		const sut2 = ChainId.from(42);
+		const sut3 = ChainId.from(7);
+
+		// ACT & ASSERT
+		expect(sut1.equals(sut2)).toBe(true);
+		expect(sut1.equals(sut3)).toBe(false);
+	});
+});
