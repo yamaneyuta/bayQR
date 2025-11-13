49f7221345df12ab1d67938e96cb7ab967e3c301
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-11-13 00:21:36 +0000

    refactor(value-object): Decimalsクラスを追加

---
+++ packages/lib-value-object/src/value-object/Decimals.ts
@@ -0,0 +1,30 @@
+import { ValueObject } from "./base/ValueObject";
+
+const brand: unique symbol = Symbol("Decimals");
+
+export class Decimals implements ValueObject<Decimals> {
+	/** 型区別用のフィールド */
+	private [brand]!: void;
+
+	private constructor(private readonly decimalsValue: number) {
+		if (decimalsValue < 0 || !Number.isInteger(decimalsValue)) {
+			throw new Error(`[195A68AE] Decimals must be a non-negative integer. ${decimalsValue}`);
+		}
+	}
+
+	public static from(decimalsValue: number): Decimals {
+		return new Decimals(decimalsValue);
+	}
+
+	public get value(): number {
+		return this.decimalsValue;
+	}
+
+	public equals(other: Decimals): boolean {
+		return this.decimalsValue === other.decimalsValue;
+	}
+
+	public toString(): string {
+		return this.decimalsValue.toString();
+	}
+}
